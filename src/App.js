bcae8f9e3b8cd71fb40ae799c69fbcb3f89d296a
Andrey Zhukov <zhukov290791@gmail.com>
2020-02-18 10:13:19 +0500

    send props to component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Car from './App/Car';
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>
-  //       Hello world!
-  //     </h1>
-  //   </div>
-  // );
-  return React.createElement(
-      'div',
-      {
-        className: 'app',
-      },
-      React.createElement(
-          'h1',
-          null,
-          'Hello world'),
-  )
+class App extends Component {
+  render() {
+    const divStyle = {
+      textAlign: 'center',
+    };
+    return (
+        <div style={divStyle}>
+          <h1>
+            First Project
+          </h1>
+          <Car name={'Ford'} year={2018} />
+          <Car name='Audi' year={2016}>
+            <p style={{color: 'red'}}>COLOR</p> </Car>
+          <Car name={'Mazda'} year={2010} />
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/App/Car.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// function Car() {
+//     return (
+//         <div>This is car component</div>
+//     )
+// }
+
+// const car = () =>
+//     <div>
+//         This is car component
+//         <strong>Test</strong>
+//     </div>;
+
+export default props => (
+    <div>
+        <h3>Car name {props.name}</h3>
+        <p>Year: <strong> {props.year}</strong></p>
+        { props.children }
+    </div>
+)
